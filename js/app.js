0ed5c2f147fdbb5a64761e25cdd808e1c3b31607
Matheus Pierre Sforça <matheussforza@gmail.com>
2018-11-07 20:08:36 -0200

    Added starter site

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+function tratarMenu(){
+    document.getElementById("menu").classList.toggle("menu-escondido");
+
+    
+    document.getElementById("hamburger-primeiro").classList.toggle("hamburger-primeiro");
+    document.getElementById("hamburger-segundo").classList.toggle("hamburger-segundo");
+    document.getElementById("hamburger-terceiro").classList.toggle("hamburger-terceiro");
+}
+
+function tratarOpcoesIdioma(){
+    document.getElementById("idiomas").classList.toggle("idiomas-escondido");
+    document.getElementById("icone-idiomas").classList.toggle("seta-cima");
+}
+
+    
+document.getElementById("hamburger-primeiro").classList.toggle("hamburger-primeiro");
+document.getElementById("hamburger-segundo").classList.toggle("hamburger-segundo");
+document.getElementById("hamburger-terceiro").classList.toggle("hamburger-terceiro");
